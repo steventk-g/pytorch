381ca97168da0442fcc03259222349f6afa22d4f
Catherine Lee <csl@fb.com>
2022-07-22 16:48:27 -0700

    changes

--- tools/stats/monitor.py
+++ tools/stats/monitor.py
@@ -2,64 +2,53 @@
 import datetime
 import json
 import signal
-import subprocess
-import sys
 import time
 from typing import Any, Dict, List
 
+import psutil  # type: ignore[import]
+import pynvml  # type: ignore[import]
 
-def add_package_to_path(package_name: str) -> None:
-    location = subprocess.run(
-        [sys.executable, "-m", "pip", "show", package_name],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-    )
-    for s in location.stdout.decode("utf-8").splitlines():
-        if "Location:" in s:
-            sys.path.append(s.split(" ")[1])
+
+def get_processes_running_python_tests() -> List[Any]:
+    python_processes = []
+    for process in psutil.process_iter():
+        try:
+            if "python" in process.name() and process.cmdline():
+                python_processes.append(process)
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            # access denied or the process died
+            pass
+    return python_processes
+
+
+def get_per_process_cpu_info() -> List[Dict[str, Any]]:
+    processes = get_processes_running_python_tests()
+    per_process_info = []
+    for p in processes:
+        info = {
+            "pid": p.pid,
+            "cmd": " ".join(p.cmdline()),
+            "cpu_percent": p.cpu_percent(),
+            "rss_memory": p.memory_info().rss,
+            "uss_memory": p.memory_full_info().uss,
+        }
+        if "pss" in p.memory_full_info():
+            # only availiable in linux
+            info["pss_memory"] = p.memory_full_info().pss
+        per_process_info.append(info)
+    return per_process_info
+
+
+def get_per_process_gpu_info(handle: Any) -> List[Dict[str, Any]]:
+    processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
+    per_process_info = []
+    for p in processes:
+        info = {"pid": p.pid, "gpu_memory": p.usedGpuMemory}
+        per_process_info.append(info)
+    return per_process_info
 
 
 if __name__ == "__main__":
-    # add_package_to_path("psutil")
-    # add_package_to_path("pynvml")
-    import psutil  # type: ignore[import]
-    import pynvml  # type: ignore[import]
-
-    def get_processes_running_python_tests() -> List[Any]:
-        python_processes = []
-        for process in psutil.process_iter():
-            try:
-                if "python" in process.name() and process.cmdline():
-                    python_processes.append(process)
-            except (psutil.NoSuchProcess, psutil.AccessDenied):
-                # access denied or the process died
-                pass
-        return python_processes
-
-    def get_per_process_cpu_info() -> List[Dict[str, Any]]:
-        processes = get_processes_running_python_tests()
-        per_process_info = []
-        for p in processes:
-            info = {
-                "pid": p.pid,
-                "cmd": " ".join(p.cmdline()),
-                "cpu_percent": p.cpu_percent(),
-                "rss_memory": p.memory_info().rss,
-                "uss_memory": p.memory_full_info().uss,
-            }
-            if "pss" in p.memory_full_info():
-                # only availiable in linux
-                info["pss_memory"] = p.memory_full_info().pss
-            per_process_info.append(info)
-        return per_process_info
-
-    def get_per_process_gpu_info(handle: Any) -> List[Dict[str, Any]]:
-        processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
-        per_process_info = []
-        for p in processes:
-            info = {"pid": p.pid, "gpu_memory": p.usedGpuMemory}
-            per_process_info.append(info)
-        return per_process_info
 
     handle = None
     try:
